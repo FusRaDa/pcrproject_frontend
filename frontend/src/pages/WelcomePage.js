3390dcb91c26a77282b7f97e44fc8e4480c65fd0
Matthew Rada <matthewr7.christopher@gmail.com>
2022-11-25 19:18:00 -0500

    welcome page

--- frontend/src/pages/WelcomePage.js
+++ frontend/src/pages/WelcomePage.js
@@ -1,8 +1,47 @@
-import React from "react";
+import React from "react"
+import Container from "react-bootstrap/Container"
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import { Link } from "react-router-dom"
 
 const WelcomePage = () => {
   return(
-    <div>welcome!</div>
+    <Container>
+      <Row>
+
+        <Col>
+          <Card>
+            <Card.Header>Frontend Features</Card.Header>
+            <ListGroup>
+              <ListGroup.Item><Link to="https://github.com/FusRaDa/pcrproject_frontend">Frontend Code</Link></ListGroup.Item>
+              <ListGroup.Item>React.js</ListGroup.Item>
+              <ListGroup.Item>Styling Powered by React-Bootstrap</ListGroup.Item>
+              <ListGroup.Item>Dynamic and Interactive Table powered by React-Table</ListGroup.Item>
+              <ListGroup.Item>Local Storage for Saving Preferences</ListGroup.Item>
+              <ListGroup.Item>Form Validation for User Experience</ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+
+        <Col>
+          <Card>
+            <Card.Header>Backend Features</Card.Header>
+            <ListGroup>
+              <ListGroup.Item>Django Rest Framework</ListGroup.Item>
+              <ListGroup.Item><Link to="https://github.com/FusRaDa/pcrproject_backend">Backend Code</Link></ListGroup.Item>
+              <ListGroup.Item>JSON Web Tokens Authentication</ListGroup.Item>
+              <ListGroup.Item>CRUD Operations</ListGroup.Item>
+              <ListGroup.Item>ForeignKey and ManytoMany Table Relations</ListGroup.Item>
+              <ListGroup.Item>User Permissions</ListGroup.Item>
+              <ListGroup.Item>Validation of Information and Security</ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+
+      </Row>
+    </Container>
   )
 }
 
